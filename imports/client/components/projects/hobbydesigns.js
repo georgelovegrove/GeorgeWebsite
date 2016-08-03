95f9115cc0e10bc6219d310e07c0179103b98c05
George Lovegrove <lovegrovegeorge@gmail.com>
2016-08-03 21:52:47 +0100

    Minor text change

--- imports/client/components/projects/hobbydesigns.js
+++ imports/client/components/projects/hobbydesigns.js
@@ -20,11 +20,11 @@ const HobbyDesigns = () => {
             <div className="project_inner_container">
              
                 <p>For this project an assortment of icons were needed to represent each hobby. To cater 
-                for this well over 100 icons were made for use in the application. A small selection is shown below. </p>
+                for this I created well over 100 icons for use in the application. A small selection is shown below. </p>
 
                 <img className="hobby_icon_selection" src="/projects/iconselection.png" />
 
-                <p>I designed the website and application from wireframe to final designs using Sketch. After design were completed
+                <p>I designed the website and application from wireframe to final designs using Sketch. After the designs were completed
                 I developed the website and lead the application design implementation using React Native. 
                 An example screen for the website homepage is shown below.</p>
 
